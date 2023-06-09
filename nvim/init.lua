9b89b051f09a5c7d5ba13e850174e79195889802
Haruto Watahiki <43723360+WATA-Haru@users.noreply.github.com>
2023-06-10 00:12:44 +0900

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,36 @@
+-- 行番号を表示
+vim.opt.number = true
+
+-- ターミナルのタイトルを表示
+vim.opt.title = true
+-- 文字コードをutf-8に変換
+vim.opt.encoding = "utf-8"
+-- Tab文字の表示幅（半角スペース4つ分）
+vim.opt.tabstop = 4
+-- 不可視文字の可視化（Tabを[^ ]と表示する）
+vim.opt.list = true
+
+vim.opt.clipboard = "unnamed"
+
+-- ESC to jk
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true })
+-- リーダーキーをスペースで設定可能に
+vim.g.mapleader = " "
+
+vim.cmd("filetype plugin indent on")
+
+
+if vim.fn.has("wsl") then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf"
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --crlf",
+      ["*"] = "win32yank.exe -o --crlf"
+    },
+    cache_enable = 0,
+  }
+end
